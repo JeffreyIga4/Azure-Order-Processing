8e8b4f9cc316fdc506546f619d73738dcad61931
Jeffrey Igala <jeffreyigala@Jeffreys-MBP.station>
2025-06-20 18:41:25 +0100

    Function POSTs to logic app URL

--- queue-function/Program.cs
+++ queue-function/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Company.Function;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +11,7 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddHttpClient<ProcessOrderFunction>();
 
 builder.Build().Run();
